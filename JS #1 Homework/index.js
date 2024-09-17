35265c290b9bfe199beca3211027124b1b1c3220
Enrikas <ramasauskasenrikas@gmail.com>
2024-09-17 08:30:39 +0300

    Finished homework

---
+++ JS #1 Homework/index.js
@@ -0,0 +1,23 @@
+/// STACIAKAMPIO PLOTAS
+function staciakampioApskaiciavimas(a, b) {
+  let plotas = a * b;
+  let perimetras = 2 * a + 2 * b;
+  let matavimoVienetas = "m";
+
+  console.log(
+    "Stačiakampio kraštinė A:" +
+      a +
+      matavimoVienetas +
+      "\n" +
+      "Stačiakampio kraštinė B: " +
+      b +
+      matavimoVienetas +
+      "\n" +
+      "Plotas: " +
+      plotas.toFixed(2) +
+      `${matavimoVienetas}² `,
+    "\n" + "Perimetras: " + perimetras.toFixed(2) + matavimoVienetas
+  );
+}
+
+staciakampioApskaiciavimas(22, 10.2);
