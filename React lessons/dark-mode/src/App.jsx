29bda527a400a8360899688aa5c51f8c07e78c53
Enrikas <ramasauskasenrikas@gmail.com>
2024-12-10 08:56:14 +0200

    Removed text

--- React lessons/dark-mode/src/App.jsx
+++ React lessons/dark-mode/src/App.jsx
@@ -15,9 +15,7 @@ function App() {
   return (
     <div className="flex flex-col gap-2 justify-center place-items-center h-[100vh]">
       <h1 className="text-4xl font-bold">Hello world!</h1>
-      <p className="font-semibold">
-        Theme is: {isDarkMode ? "Dark" : "Light"}{" "}
-      </p>
+      <p className="font-semibold">Theme is: {isDarkMode ? "Dark" : "Light"}</p>
       <button
         className="bg-gray-500 hover:bg-gray-400 p-2 rounded font-semibold"
         onClick={() => setIsDarkMode((prev) => !prev)}
