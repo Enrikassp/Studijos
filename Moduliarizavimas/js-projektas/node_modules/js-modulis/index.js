5b9a269e488e73d205f44b55e53814f4eb14a8ce
Enrikas <ramasauskasenrikas@gmail.com>
2025-01-21 09:28:20 +0200

    Many days push

---
+++ Moduliarizavimas/js-projektas/node_modules/js-modulis/index.js
@@ -0,0 +1,15 @@
+import { color } from "console-log-colors";
+
+export function log(message, variant = "info") {
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("lt-LT");
+
+  let formattedMessage;
+
+  if (variant === "info") formattedMessage = color.yellow(message);
+  else if (variant === "error") formattedMessage = color.red(message);
+  else if (variant === "warning") formattedMessage = color.magenta(message);
+  else formattedMessage = message;
+
+  console.log(`[${color.red(formattedDate)}]: ${formattedMessage}`);
+}
